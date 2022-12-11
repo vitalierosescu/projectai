fc9959b488c6e44a499bee9ab82a6946a95156b7
vitaliethrill <44435220+vitaliethrill@users.noreply.github.com>
2022-12-11 18:32:56 +0100

    production build test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
+console.log('This shit do be working tho')
